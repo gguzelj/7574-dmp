5487087ce606ce46b07f8b58859a7645e2468208
German Guzelj <german.guzelj@gmail.com>
2018-05-13 01:19:59 -0300

    adding safe logging

--- src/lib/log/logger.c
+++ src/lib/log/logger.c
@@ -1,4 +1,16 @@
-//
-// Created by German Guzelj on 12/5/18.
-//
+#include <stdio.h>
+#include <string.h>
+#include "logger.h"
 
+void init_logger(char* name) {
+    logConfig.name = name;
+    logConfig.lockFd = open_lockfile(LOG_FILE);
+}
+
+void safelog(char *logMessage) {
+    acquire_lock(logConfig.lockFd);
+    char msg[200];
+    sprintf(msg, "[%s] - %s\n", logConfig.name, logMessage);
+    write_lockfile(logConfig.lockFd, msg, strlen(msg));
+    release_lock(logConfig.lockFd);
+}
--- src/lib/log/logger.h
+++ src/lib/log/logger.h
@@ -1,8 +1,18 @@
-//
-// Created by German Guzelj on 12/5/18.
-//
-
 #ifndef INC_7574_DMP_LOGGER_H
 #define INC_7574_DMP_LOGGER_H
+
+#include "../lockfile/lockfile.h"
+
+#define LOG_FILE    "/tmp/dist_2017.log"
+
+typedef struct loggerConfig {
+    char* name;
+    int lockFd;
+} loggerConfig;
+
+loggerConfig logConfig;
+
+void init_logger(char* name);
+void safelog(char *logMessage);
 
 #endif //INC_7574_DMP_LOGGER_H
