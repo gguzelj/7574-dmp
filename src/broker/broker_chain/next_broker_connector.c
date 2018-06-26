8673a8fdfe3b303471d12efa9d8c6fc418a1d768
German Guzelj <german.guzelj@gmail.com>
2018-06-26 00:53:53 -0300

    adding log

--- src/broker/broker_chain/next_broker_connector.c
+++ src/broker/broker_chain/next_broker_connector.c
@@ -19,6 +19,7 @@ int main(int argc, char **argv) {
             continue;
         }
         dispatch_to_next_broker(request);
+        safelog("another round...");
     } while (config.running);
     safelog("EXIT");
 }
